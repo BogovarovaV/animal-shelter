f5cf51762f900b8106bb6bad2e6f5bcb981076d5
Диана <diana_yudina@mail.ru>
2022-05-30 19:43:39 +0500

    Изменения 30.05.22: Изменила статус пользователя по умолчанию, добавила новые статусы, дропнула базу и пересоздала основную таблицу. Создала сущность Report.

--- src/main/resources/liquibase/scripts/migrations.sql
+++ src/main/resources/liquibase/scripts/migrations.sql
@@ -9,8 +9,8 @@ create table Client
     name         varchar(255) NOT NULL,
     phone_number varchar(255) NOT NULL,
     email        varchar(255) NOT NULL,
-    status       varchar(255) NOT NULL DEFAULT 'REQUESTED'
+    status       varchar(255) NOT NULL DEFAULT 'USER'
 );
 
 -- changeset Diana:2
-CREATE INDEX user_data_idx ON Client (user) WHERE status = 'REQUESTED';
+CREATE INDEX user_data_idx ON Client (user) WHERE status = 'USER';
